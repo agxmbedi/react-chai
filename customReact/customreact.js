4452de3169e9244f7193a124519bc282397f19e4
Asher-xx <agamjot2413.be21@chitkara.edu.in>
2024-05-08 15:13:28 +0530

    custom react

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,10 +1,18 @@
 function customRender(reactElement,container){
-    const domElement = document.createElement
-    (reactElement.type)
-    domElement.innerHTML =reactElement.children
-    domElement.setAttribute('href',reactElement.props.href)
-    domElement.setAttribute('target',reactElement.props.target)
+    // const domElement = document.createElement
+    // (reactElement.type)
+    // domElement.innerHTML =reactElement.children
+    // domElement.setAttribute('href',reactElement.props.href)
+    // domElement.setAttribute('target',reactElement.props.target)
 
+    // container.appendChild(domElement)
+    const domElement=document.createElement
+    (reactElement.type)
+    domElement.innerHTML=reactElement.children
+    for (const prop in reactElement.props) {
+       if(prop==='children') continue;
+       domElement.setAttribute(prop,reactElement.props[prop])
+    }
     container.appendChild(domElement)
 }
 
@@ -13,9 +21,9 @@ const reactElement= {
     props:{
         href:'https://google.com',
         target:'_blank'
-    },
-    children : 'Click me to visit google'
+    }, 
+    children : "Click me to visit google"
 }
 const mainContainer= document.querySelector('#root')
 
-customRender(reactElement,mainContainer)
+customRender(reactElement,mainContainer) 
