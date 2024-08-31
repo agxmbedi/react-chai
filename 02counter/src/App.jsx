9ac674e2d0935aa147665492eed70a405490e850
Agamjot Bedi <111156687+agxmbedi@users.noreply.github.com>
2024-08-31 18:23:25 +0000

    counter by hooks

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,27 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
-
-
-let [counter,setCounter] = useState(15)
+  // we get 2 things in the form of an arraY,
+  // first is name (any name),SetCounter is a function.
+  let [counter,setCounter] = useState(15)
+ // useState can have any value initinally 
 
  // let counter =5
-  
   const addValue=()=>{
-   
-    //counter=counter+1 
+     //counter=counter+1 
     setCounter(counter+1)
-    
-  }
+    }
 const removeValue=()=>{
   setCounter(counter-1)
 }
-
+// by using hooks we can make changes to UI directly and ek hi baar meh kr skte hai.
 // value shloud not go below 0 and not above 20.
   return (
     <>
-     
       <h1>Vite + React</h1>
       <h2>counter value :{counter}</h2>
       <button
@@ -32,9 +28,7 @@ const removeValue=()=>{
       <br/>
       <button
       onClick={removeValue} >Remove Value{counter}</button>
-     
-    
-    </>
+     </>
   )
 }
 
