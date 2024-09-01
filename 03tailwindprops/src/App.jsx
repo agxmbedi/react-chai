bb54d953a3817c5db500e25e8b75157f71134f40
agxmbedi <pavitsinghaps33@gmail.com>
2024-09-01 18:36:45 +0530

    using props in components

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -3,12 +3,18 @@ import './App.css'
 import Card from './components/Card'
 function App() {
   const [count, setCount] = useState(0)
+  let myObj={
+    username:"agam",
+    age:22
+  }
+let newArr=[1,2,3];
 
   return (
     <>
      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'> Tailwiind test</h1>
-    <Card/>
-    <Card/>
+    <Card btnText="click me and find out"/>
+
+    <Card username="syna" />
     </>
   )
 }
--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
 
-function Card(props) {
+function Card({username="Agam is great ",btnText="visit me please"}) {
+ // console.log("props", props);
+  // props can take any values
+ // console.log(props.username);
+ //console.log(username);
   return (
-    <div class="relative h-[400px] w-[300px] rounded-md">
+    <div className="relative h-[400px] w-[300px] rounded-md">
     <img
       src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
       alt="AirMax Pro"
-      class="z-0 h-full w-full rounded-md object-cover"
+      className="z-0 h-full w-full rounded-md object-cover"
     />
-    <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
-    <div class="absolute bottom-4 left-4 text-left">
-      <h1 class="text-lg font-semibold text-white">Delba</h1>
-      <p class="mt-2 text-sm text-gray-300">
+    <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+    <div className="absolute bottom-4 left-4 text-left">
+      <h1 className="text-lg font-semibold text-white">{username}</h1>
+      <p className="mt-2 text-sm text-gray-300">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
         debitis?
       </p>
-      <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-        View Profile →
-      </button>
+      <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+        {/* {btnText || "visit me "} (agar by chance yeh property pass hi na ki ho 
+     aise default value aise dete hai and 2nd method is to give direct in the beginnings) */}
+        {btnText}
+      </button> 
     </div>
   </div>
   )
