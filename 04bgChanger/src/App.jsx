00f15f59b8cde5c12034af1dc3cdcb25c5e68d76
agxmbedi <pavitsinghaps33@gmail.com>
2024-09-01 19:11:47 +0530

    mini bg changer project

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+
+function App() {
+ const [color,setColor]=useState("olive") 
+
+  return (
+    <div className="w-full h-screen duration-200"
+    style={{backgroundColor :color}}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl"> 
+
+          <buttom onClick={()=>setColor("red")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor:"red"}} >Red</buttom>
+
+          <buttom onClick={()=>setColor("green")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor:"green"}} >Green</buttom>
+
+          <buttom onClick={()=>setColor("Blue")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor:"blue"}} >Blue</buttom>
+
+          <buttom onClick={()=>setColor("black")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor:"black"}} >Black</buttom>
+
+          <buttom onClick={()=>setColor("pink")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor:"pink"}} >Pink</buttom>
+         </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
