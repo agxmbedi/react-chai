77e588b16e326852e670b1132bcdcafb16a65679
Agamjot Bedi <111156687+agxmbedi@users.noreply.github.com>
2024-09-03 16:25:01 +0000

    password generator start & end length

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -23,6 +23,7 @@ function App() {
   const copyPasswordToClipboard=useCallback(()=>{
     passwordRef.current?.select()
     passwordRef.current?.setSelectionRange(0,10);
+    
     window.navigator.clipboard.writeText(password);
   },[password]);
 
@@ -53,7 +54,7 @@ function App() {
         <input 
         type="range"
         min={6}
-        max={100}
+        max={50}
         value={length}
          className='cursor-pointer'
          onChange={(e) => {setLength(e.target.value)}}
