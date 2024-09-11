d9f2cd7b0c35d999ec22685170a188b333c59201
agxmbedi <pavitsinghaps33@gmail.com>
2024-09-11 18:55:06 +0530

    context api

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
 
   return (
-    <>
+    <UserContextProvider>
       <h1>Agam ka project</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
--- 08miniContext/src/context/UserContext.js
+++ 08miniContext/src/context/UserContext.js
@@ -1,12 +1,8 @@
 import React from 'react'
 
 const UserContext=React.createContext()
-function UserContext() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default UserContext
+
+export default UserContext;
+
+
