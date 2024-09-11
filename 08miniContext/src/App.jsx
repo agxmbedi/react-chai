26b414c4fc27528257f57afdde13ba6484b546a5
agxmbedi <pavitsinghaps33@gmail.com>
2024-09-11 16:14:08 +0530

    context api

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1>Agam ka project</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserContext=React.createContext()
+function UserContext() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserContext
