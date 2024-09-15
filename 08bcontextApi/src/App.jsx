05f9dedf44705ace699400f4b467b1af42d1087c
agxmbedi <pavitsinghaps33@gmail.com>
2024-09-15 19:18:34 +0530

    understanding the context api works

---
+++ 08bcontextApi/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Counter from './components/Counter'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-purple-300 text-3xl px-2 py-2 '>understanding the context api</h1>
+      <Counter/>
+    </>
+  )
+}
+
+export default App
---
+++ 08bcontextApi/src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Counter= ()=>{
+    return(
+        <div>
+            <button className='bg-green-300'>Increment</button>
+            <br/>
+            <button className='bg-green-300'>Decrement</button>
+        <br/>
+            <button>Fuck you react</button>
+        </div>
+    )
+}
+export default Counter
