847e8ebd6a9c2cac7e198cc84734327c47c8f4bd
agxmbedi <pavitsinghaps33@gmail.com>
2024-09-16 18:01:13 +0530

    context API

--- 08bcontextApi/src/components/Counter.jsx
+++ 08bcontextApi/src/components/Counter.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useContext } from 'react'
+import { CounterContext } from './Context/Context'
 
 const Counter= ()=>{
+    const counterContext=useContext(CounterContext)
     return(
         <div>
-            <button className='bg-green-300'>Increment</button>
-            <br/>
-            <button className='bg-green-300'>Decrement</button>
+        <button onClick={()=> counterContext.setCount(counterContext.count + 1)} className="bg-gray-500
+         hover:bg-blue-700
+          text-white font-bold py-2 px-4 border
+           border-gray-700 rounded">
+                Increment
+                </button>
+            
+            <button  onClick={()=> counterContext.setCount(counterContext.count - 1)}
+            className="bg-gray-500
+         hover:bg-blue-700
+          text-white font-bold py-2 px-4 border
+           border-gray-700 rounded">
+                Decrement
+                </button>
+           
         <br/>
-            <button>Fuck you react</button>
         </div>
     )
 }
