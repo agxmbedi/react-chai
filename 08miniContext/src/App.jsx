645edda321e41df9b655ba9360ae01623c6cdf73
agxmbedi <pavitsinghaps33@gmail.com>
2024-09-17 18:12:34 +0530

    Basic understanding of React

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import UserContextProvider from './context/UserContextProvider'
 import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <UserContextProvider>
      <h1> React and Context API</h1>
-    </>
+     <Login/>
+     <Profile/>
+    </UserContextProvider>
   )
 }
 
--- 08miniContext/src/context/UserContextProvider.jsx
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import UserContext from "./UserContext";
 
-const UserContextProvider=()=>{
-
+const UserContextProvider=({children})=>{
+    const [user,setUser]=useState(null)
+    return (
+        <UserContext.Provider value={{user,setUser}}>
+        {children}
+        </UserContext.Provider>
+    )
 }
 export default UserContextProvider
