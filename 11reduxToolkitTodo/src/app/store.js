13f94ed454ac7d94a69c89cf5a577df8901aa626
agxmbedi <pavitsinghaps33@gmail.com>
2024-09-18 23:23:44 +0530

    Todo using redux

--- 11reduxToolkitTodo/src/app/store.js
+++ 11reduxToolkitTodo/src/app/store.js
@@ -1,5 +1,5 @@
-import configureStore from '@reduxjs/toolkit';
-import todoReducer from '../features/todo/todoSlice';
+import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../features/todo/todoSlice.js';
 
 export const store = configureStore({
     reducer: todoReducer
--- 11reduxToolkitTodo/src/components/AddTodo.jsx
+++ 11reduxToolkitTodo/src/components/AddTodo.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
-import useDispatch from 'react-redux'
-import addTodo from '../features/todo/todoSlice'
+import {useDispatch} from 'react-redux'
+import {addTodo} from '../features/todo/todoSlice'
 
 function AddTodo() {
 
