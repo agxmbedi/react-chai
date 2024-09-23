96c32f284e62ad53496640a533edac470b5da016
agxmbedi <pavitsinghaps33@gmail.com>
2024-09-23 18:23:32 +0530

    Makng components

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,14 +1,34 @@
+import React,{ useEffect, useState } from 'react';
 import './App.css'
+import {useDispatch} from 'react-redux'
 import config from './config/config';
+import authService from './appwrite/auth';
+import {login,logout} from './store/authSlice'
+
+
 
 function App() {
-  
+ const [loading,setLoading]=useState(true);
+ const dispatch = useDispatch()
+ 
+ 
+ useEffect(()=>{
+  authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else {
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
 
-  return (
-    <>
-      <h1> Mega Project</h1>
-    </>
-  )
+ },[])
+
+  return !loading ?(
+    <div className='min-h-sc'>test </div>
+  ) : null
 }
 
 export default App
